35577cf82384b91e18794af203394314519e1cd8
vudongpham <vudong.pham27@gmail.com>
2024-11-14 20:05:09 +0100

    update interface

--- map.js
+++ map.js
@@ -1,25 +1,38 @@
-var map = L.map('map').setView([55.680,17.302], 6);
+var map = L.map('map').setView([55.680,17.302], 5);
 
-
-
-L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
+var osm = L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
     maxZoom: 19,
     attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
-}).addTo(map);
+});
+
+var Esri_WorldImagery = L.tileLayer('https://server.arcgisonline.com/ArcGIS/rest/services/World_Imagery/MapServer/tile/{z}/{y}/{x}', {
+    maxZoom: 19,
+	attribution: 'Tiles &copy; Esri &mdash; Source: Esri, i-cubed, USDA, USGS, AEX, GeoEye, Getmapping, Aerogrid, IGN, IGP, UPR-EGP, and the GIS User Community'
+});
+
+var baseMaps = {
+    "OpenStreetMap": osm,
+    "Esri satellite": Esri_WorldImagery,
+};
+
+Esri_WorldImagery.addTo(map)
+L.control.layers(baseMaps).addTo(map);
 
 
 var regularStyle = {
     stroke: true,
-    fillOpacity: 0.2,
-    color: '#f5f542',
-    opacity: 0.7,
+    fillOpacity: 0.3,
+    color: '#000000',
+    fillColor: '#00c800',
+    opacity: 1,
     weight: 1
     };
     
 var highlightStyle = {
     stroke: true,
     fillOpacity: 0,
-    color: '#ff0202',
+    color: '#ff0000',
+    fillColor: 'green',
     opacity: 1,
     weight: 4
     };
@@ -31,17 +44,11 @@ vector.setStyle(regularStyle);
 vector.bringToFront()
 }.bind(this));
 
+let currentChart = null; 
+
 function checkHover(feature, layer) {
     layer.bindTooltip(
-        "<b>Number of scenes</b><br>" +
-        "Landsat 5: " + layer.feature.properties.Landsat5 + "<br>" +
-        "Landsat 7: " + layer.feature.properties.Landsat7 + "<br>" +
-        "Landsat 7: " + layer.feature.properties.Landsat8 + "<br>" +
-        "Landsat 9: " + layer.feature.properties.Landsat9 + "<br>" +
-        "Sentinel-2A: " + layer.feature.properties.Sentinel2A + "<br>" +
-        "Sentinel-2B: " + layer.feature.properties.Sentinel2B + "<br>" +
-        "Total scenes : " + layer.feature.properties.Total + "<br>" 
-    )
+        `<b>${layer.feature.properties.Tile_ID}</b><br>Click for details`)
     layer.on({
         mouseover: function(e) {
         layer.setStyle(highlightStyle)
@@ -50,6 +57,47 @@ function checkHover(feature, layer) {
         layer.setStyle(regularStyle)
         },
     click: function(e) {
+        // content =   `<strong>X:</strong>${layer.feature.properties.Tile_X}<br>
+        //             <strong>Y:</strong>${layer.feature.properties.Tile_Y}<br>`;
+        // content = `<br>a<br>a<br>a<br>a<br>a<br>a<br>a<br>a`
+        // document.getElementById('datatext').innerHTML = content
+
+        if (currentChart) {
+            currentChart.destroy(); // Destroy the existing chart instance
+        }
+        const xValues = ["Landsat 5", "Landsat 7", "Landsat 8", "Landsat 9", "Sentinel-2A", "Sentinel-2A"];
+        const yValues = [layer.feature.properties.Landsat5,
+                        layer.feature.properties.Landsat7,
+                        layer.feature.properties.Landsat8,
+                        layer.feature.properties.Landsat9,
+                        layer.feature.properties.Sentinel2A,
+                        layer.feature.properties.Sentinel2B
+                    ];
+        const barColors = ["#e6e035", "#b4d121","#6ed1ff","#2fc4b8","#9334e0","#de2f95"];
+
+        currentChart = new Chart(document.getElementById("myChart"), {
+            type: "bar",
+            data: {
+            labels: xValues,
+            datasets: [{
+                backgroundColor: barColors,
+                data: yValues
+                    }]},
+            options: {
+            legend: {display: false},
+            title: {
+                display: true,
+                text: layer.feature.properties.Tile_ID + " - Total scenes: "  + layer.feature.properties.Total.toString(),
+                animation: {
+                    duration: 0 // This disables animations
+                    }
+                },
+            maintainAspectRatio: false
+            }
+        });
         }
     });
-    }
+}
+
+
+
